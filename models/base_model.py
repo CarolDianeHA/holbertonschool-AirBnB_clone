c8eaa7aa0ef927fbf3dcba89939339f5e8dab502
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-03-07 22:27:11 -0500

    Removing global variable dtm and correcting some docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""Class BaseModel"""
+"""Class BaseModel."""
 
 import uuid
 from datetime import datetime
 
-# date format
-dtm = "%Y-%m-%dT%H:%M:%S.%f"
-
 
 class BaseModel:
-    """Class that defines all common attributes/methods for other classes"""
+    """Class that defines all common attributes/methods for other classes."""
+
     def __init__(self):
-        """
-        Initialization of the objects:
-        id: unique identification
-        created_at: datetime - datetime when an instance is created
-        updated_at: datetime - datetime when an instance is modified
+        """Instances of the base model class.
+
+        Args:
+            id: unique identification.
+            created_at: datetime - datetime when an instance is created.
+            updated_at: datetime - datetime when an instance is modified.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """Prints class name, id, and dictionary representation"""
+        """Print class name, id, and dictionary representation."""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
-        """Updates attribute with the current datetime"""
+        """Update attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns dictionary representation of the instance's attributes"""
+        """Return dictionary representation of the instance's attributes."""
         dict = self.__dict__.copy()
         dict['__class__'] = type(self).__name__
         dict['created_at'] = dict['created_at'].isoformat()
