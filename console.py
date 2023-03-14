1c3ac0e3b31093ce45ee00dc62fab38d27b8372f
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-13 20:41:34 -0400

    Show function.

--- console.py
+++ console.py
@@ -3,6 +3,19 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import models
+import shlex
+
+
+classGroup = {"Amenity": Amenity, "BaseModel": BaseModel,
+              "City": City, "Place": Place, "Review": Review,
+              "State": State, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -25,11 +38,23 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Create a new object."""
         new = BaseModel()
-        print(new)
+        print(new.id)
 
     def do_show(self, arg):
         """Show name and id of the instance."""
-        pass
+        args_list = arg.split()
+        if args_list[0] == "":
+            print("** class name missing **")
+        elif args_list[0] not in classGroup.keys():
+            print("** class doesn't exist **")
+        elif len(args_list) < 2:
+            print("** instance id missing **")
+        else:
+            id_object = "{}.{}".format(args_list[0], args_list[1])
+            if id_object not in models.storage.all():
+                print("** no instance found **")
+            else:
+                print(models.storage.all()[id_object])
 
     def do_destroy(self, arg):
         """Delete a given instance."""
