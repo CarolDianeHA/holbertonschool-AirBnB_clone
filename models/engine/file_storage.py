3281a9d9e5360f8b6b09a2e2b1fff7861320e8af
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-14 22:19:01 -0400

    Importing classes.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@
 import json
 import os
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.user import User
+from models.review import Review
 
 
 class FileStorage:
