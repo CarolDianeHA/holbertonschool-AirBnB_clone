a059495fa6b140901e02b7924b04e7b450b68a4e
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-16 20:29:52 -0400

    Updating basemodel.

--- models/base_model.py
+++ models/base_model.py
@@ -17,20 +17,18 @@ class BaseModel:
             created_at: datetime - datetime when an instance is created.
             updated_at: datetime - datetime when an instance is modified.
         """
-        if args:
-            pass
-        elif kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(val, timeformat))
+                elif key != '__class__':
+                    setattr(self, key, val)
 
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -46,7 +44,7 @@ class BaseModel:
     def to_dict(self):
         """Return dictionary representation of the instance's attributes."""
         dict = self.__dict__.copy()
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
+        dict['__class__'] = self.__class__.__name__
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
         return dict
