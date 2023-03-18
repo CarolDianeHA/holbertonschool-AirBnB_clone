53a983122433fce89dc4b41f59fba40780907cae
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-18 11:45:09 -0400

    Test city update.

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,40 +1,17 @@
-#!/usr/bin/python3
-"""Test for City Class."""
 import unittest
 from models.city import City
-from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
-    """Test instances and methods from City class."""
 
-    a = City()
-
-    def test_class_exists(self):
-        """Test if class exists."""
-        res = "<class 'models.city.City'>"
-        self.assertEqual(str(type(self.a)), res)
-
-    def test_inheritance(self):
-        """Test inheritance."""
+    def test_state_id(self):
         city = City()
-        self.assertIsInstance(city, BaseModel)
+        self.assertEqual(city.state_id, "")
 
-    def test_name_attribute(self):
-        """Test the attribute name."""
+    def test_name(self):
         city = City()
-        self.assertTrue(hasattr(self.city, 'name'))
+        self.assertEqual(city.name, "")
 
-    def test_id_attribute(self):
-        """Test if City has a state id attribute."""
-        self.assertTrue(hasattr(self.city, 'state id'))
 
-    def test_to_dict_method(self):
-        """Test if to dict method produces dictionary."""
-        city_dict = self.city.to_dict()
-        self.assertTrue(isinstance(city_dict, dict))
-
-    def test_str_method(self):
-        """Test if __str__ mehtod produces string representation"""
-        city_str = str(self.city)
-        self.assertTrue(isinstance(city_str, str))
+if __name__ == '__main__':
+    unittest.main()
