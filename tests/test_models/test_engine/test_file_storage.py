413a6d07c8d23762d7a0fd7c6259caa86b74ad55
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-18 12:00:22 -0400

    Test file storage update.

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,18 +1,49 @@
-#!/usr/bin/python3
-""" Module of Unittests """
 import unittest
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models import storage
 import os
-import json
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.user import User
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
-class FileStorageTests(unittest.TestCase):
-    """ Suite of File Storage Tests """
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.file_path = "test_file.json"
+        self.storage = FileStorage()
+        self.storage._FileStorage__file_path = self.file_path
+        self.bm1 = BaseModel()
+        self.am1 = Amenity()
+        self.ct1 = City()
+        self.pl1 = Place()
+        self.st1 = State()
+        self.ur1 = User()
+        self.rv1 = Review()
 
-    my_model = BaseModel()
+    def tearDown(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
-    def testClassInstance(self):
-        """ Check instance """
-        self.assertIsInstance(storage, FileStorage)
+    def test_all(self):
+        self.assertEqual(type(self.storage.all()), dict)
+        self.assertIn('BaseModel.' + self.bm1.id, self.storage.all())
+
+    def test_new(self):
+        self.storage.new(self.am1)
+        key = 'Amenity.' + self.am1.id
+        self.assertIn(key, self.storage._FileStorage__objects)
+
+    def test_save_reload(self):
+        self.storage.new(self.ct1)
+        self.storage.save()
+        self.storage._FileStorage__objects = {}
+        self.storage.reload()
+        key = 'City.' + self.ct1.id
+        self.assertIn(key, self.storage.all())
+
+
+if __name__ == '__main__':
+    unittest.main()
