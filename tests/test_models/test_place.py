4b6dcca3c182121d89d59352c8fc0730aee0f9c5
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-18 11:29:16 -0400

    Test place.

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,12 +2,16 @@
 """Test for Place Class."""
 import unittest
 from models.place import Place
+from models.base_model import BaseModel
 
 
 class TestPlace(unittest.TestCase):
     """Test instances and methods from Place class."""
 
     a = Place()
+
+    def setUp(self):
+        self.a = Place()
 
     def test_class_exists(self):
         """Test if class exists."""
