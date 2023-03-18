64d7898cc554ddc60ebe1673ddb891a6aac0cd03
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-18 10:52:00 -0400

    Updating Test User.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@ import unittest
 from models.user import User
 import datetime
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -15,3 +16,14 @@ class TestUser(unittest.TestCase):
         """Test if class exists."""
         res = "<class 'models.user.User'>"
         self.assertEqual(str(type(self.a)), res)
+
+    def test_inheritance(self):
+        """Tests inheritance."""
+        user = User()
+        self.assertIsInstance(User, BaseModel)
+
+    def test_name_attribute(self):
+        """Tests the name attribute."""
+        user = User()
+        self.assertTrue(hasattr(User, 'name'))
+        self.assertEqual(User.name, '')
