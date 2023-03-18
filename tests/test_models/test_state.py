242774e07d66759fdb83441ddffa9527ca817500
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-18 11:49:52 -0400

    Test state update.

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python3
-"""Test for State Class."""
 import unittest
 from models.state import State
 
 
 class TestState(unittest.TestCase):
-    """Tests instances and methods from State class."""
 
-    a = State()
-
-    def test_class_exists(self):
-        """Test if class exists."""
-        res = "<class 'models.state.State'>"
-        self.assertEqual(str(type(self.a)), res)
+    def test_name(self):
+        state = State()
+        self.assertEqual(state.name, "")
 
 
 if __name__ == '__main__':
